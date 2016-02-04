010a817ebff874373e07d85dbb49be5da3f6a0e5
spluton <spluton@mail.ru>
2016-02-04 14:54:25 +0100

    Create LEGOchar.h

---
+++ LEGOchar.h
@@ -0,0 +1,21 @@
+#ifndef __LEGOCHAR.H__
+#define __LEGOCHAR.H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#endif
+
+#ifndef ROTR
+# define ROTR(x,n) (((uintmax_t)(x) >> (n)) | ((uintmax_t)(x) << ((sizeof(x) * 8) - (n))))
+#endif
+#ifndef MIN
+# define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#endif
+#ifndef MAX
+# define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#endif
+#ifndef BSWAP_32
+#define BSWAP_32(x) \
+     ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) | \
+      (((x) & 0x0000ff00) <<  8) | (((x) & 0x000000ff) << 24))
+#endif
